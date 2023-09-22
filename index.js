ad0854f6956349ff022255164e616f1d68babd51
basileu <basil.boli@yahoo.com>
2023-09-22 22:45:27 +0300

    Ex 3.22
    Signed-off-by: basileu <basil.boli@yahoo.com>

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
-const Person = require('./models/person');
+const Person = require('./models/person')
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+let persons = []
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-    console.log("ErrorHandler called");
+    console.log("ErrorHandler called")
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
-      }
+    }
 
     next(error)
 }
@@ -48,7 +27,7 @@ app.use(express.static('build'))
 
 
 app.use(express.json())
-app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]'))
 
 // const generateId = () => {
 //     const maxId = 65535;
@@ -59,13 +38,13 @@ app.use(morgan(':method :url :status :response-time ms - :res[content-length]'))
 
 // POST
 app.post('/api/persons', (request, response, next) => {
-    console.log("POST handler called");
+    console.log("POST handler called")
     const body = request.body
 
     // console.log(request.headers)
     // console.log(body.name);
 
-    if (body.name == null || body.number == null) {
+    if (body.name === null || body.number === null) {
         return response.status(400).json({
             error: 'Name or number missing'
         })
@@ -95,13 +74,9 @@ app.post('/api/persons', (request, response, next) => {
         response.json(savedPer)
         persons = persons.concat(savedPer)
 
-    }).catch((err) => next(err));
+    }).catch((err) => next(err))
 })
 
-// GET
-// app.get('/api/persons', (request, response) => {
-//     response.json(persons)
-// })
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
@@ -109,59 +84,50 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/info', (request, response) => {
-    const timestamp = new Date();
+    const timestamp = new Date()
     response.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <br/> ${timestamp}`);
+    <br/> ${timestamp}`)
     // response.json(persons)
 })
 app.get('/api/persons/:id', (request, response, next) => {
     // const id = Number.parseInt(request.params.id)
 
-    // const person = persons.find(pers => pers.id == id)
-    // if (person) {
-    //     response.json(person)
-    // } else {
-    //     response.statusMessage = "Status message: id not found";
-    //     response.status(404).end()
-    // }
+
     Person.findById(request.params.id)
-    .then(pers => {
-      if (pers) {
-        response.json(pers)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+        .then(pers => {
+            if (pers) {
+                response.json(pers)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
-  
+
     const person = {
-      name: body.content,
-      number: body.number,
+        name: body.content,
+        number: body.number,
     }
-  
+
     Person.findByIdAndUpdate(request.params.id, person, { new: true })
-      .then(updatedPerson => {
-        response.json(updatedPerson)
-      })
-      .catch(error => next(error))
-  })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    // const id = Number(request.params.id)
-    // persons = persons.filter(pers => pers.id !== id)
-    // response.status(204).end()
 
     Person.findByIdAndRemove(request.params.id)
-        .then(result => {
+        .then(() => {
             response.status(204).end()
         })
         .catch(error => next(error))
 })
-app.use(errorHandler);
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
--- models/person.js
+++ models/person.js
@@ -4,32 +4,32 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log("url is: " + url);
+console.log("url is: " + url)
 console.log('connecting to', url)
 
 mongoose.connect(url)
 
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3
-  },
-  number: String,
+    name: {
+        type: String,
+        minLength: 3
+    },
+    number: String,
 })
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 
